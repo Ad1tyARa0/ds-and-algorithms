59727625e87f89865f30ea633a979e4e234bc346
Aditya S Rao <aditya.s.rao12@gmail.com>
2022-06-05 21:08:28 +0530

    Practice question 1 solved.

---
+++ practice/Zoo.ts
@@ -0,0 +1,33 @@
+const checkZoos = (payload: string) => {
+  if (payload.length > 20) {
+    return "No";
+  }
+
+  let result_obj = {
+    z: 0,
+    o: 0,
+  };
+
+  for (let i = 0; i < payload.length; i++) {
+    if (payload[i] === "z") {
+      result_obj.z += 1;
+    }
+
+    if (payload[i] === "o") {
+      result_obj.o += 1;
+    }
+  }
+
+  const y = result_obj.o;
+  const x = result_obj.z * 2;
+
+  console.log(y, x);
+
+  if (x == y) {
+    return "Yes";
+  } else {
+    return "No";
+  }
+};
+
+console.log(checkZoos("zzzooooooo"));
