f19044820df88bfd41d35799558d9642bc35c0ac
Aditya S Rao <aditya.s.rao12@gmail.com>
2022-06-14 15:42:29 +0530

    Started merge sort.

---
+++ sorting-algorithms/MergeSort.ts
@@ -0,0 +1,14 @@
+/**
+ * Create an empty array, take a look at the smallest values in each input array.
+ * While there are still values we havent looked at.
+ *  - If the value in the first array is smaller than the value in the second array, push the value in the first array.
+ *  - If the value in the first array is larger than the value in the second  array, push the value in the second array.
+ *  - Once we exhause one array, push in all remainin values from the other array.
+ * @param arr
+ * @returns
+ */
+const mergeSort = (arr: Array<number>): Array<number> => {
+  let result = [...arr];
+
+  return result;
+};
